c4cd1f19f332747619d77dcd184aee65bb54a970
Kyi Pyar <kyipyarlwinlkw@gmail.com>
2025-05-07 21:34:29 +0630

    multiple component

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,25 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <v-app>  
+      <v-app-bar height="50" width="90%" color="primary">
+        <router-link to="/" >Home</router-link>
+        <v-spacer></v-spacer>
+        <router-link to="/about" >About</router-link>
+      </v-app-bar>
+      <v-main>
+        <router-view/>
+    </v-main>  
+  </v-app>
+
+
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-nav {
-  padding: 30px;
-}
+export default {
+  name: 'App',
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+  data: () => ({
+    //
+  }),
 }
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
